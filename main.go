d4e28151d7266b8828d3f718cce7aeba2fc958b0
Aaron Kyungmin Seo <aaronkmseo@gmail.com>
2022-02-16 15:14:06 -0800

    i should get a remote ssh going on

--- main.go
+++ main.go
@@ -4,8 +4,11 @@ import (
 	"bufio"
 	"flag"
 	"io"
+	"log"
 	"os"
 	"runtime"
+	"strconv"
+	"sync"
 )
 
 // Declaration of global constants
@@ -37,73 +40,121 @@ func init() {
 // Just realized, we want compress to happen concurrent with read...
 // This current strategy doesn't do that... We'll benchmark later.
 func main() {
-	// Parse arguments
 	flag.Parse()
 
 	r := read()
 
-	compressOutbounds := make([]<-chan block, processes)
-	for p := 0; p < processes; p++ {
-		compressOutbounds[p] = compress(r)
+	c := compress(r)
+
+	for output := range c {
+		write(output)
 	}
 
-	for c := range mergeSlice(compressOutbounds) {
-		write(c)
-	}
+	/*
+		compressOutbounds := make([]<-chan *block, processes)
+		for p := 0; p < processes; p++ {
+			compressOutbounds[p] = compress(r)
+		}
+
+		for c := range mergeSlice(compressOutbounds) {
+			write(c)
+		}
+	*/
 }
 
 // Read stage
-func read() <-chan block {
-	out := make(chan block)
+func read() <-chan *block {
+	log.Println("reading")
+	out := make(chan *block)
 
-	// Start reading input from Stdin in byte array buffers with BLOCK_SIZE
-	inputBuffer := make([]byte, BLOCK_SIZE, BLOCK_SIZE)
+	go func() {
+		// Start reading input from Stdin in byte array buffers with BLOCK_SIZE
+		inputBuffer := make([]byte, BLOCK_SIZE, BLOCK_SIZE)
 
-	var numBytesTotal int
-	var numBlocks int
+		var numBytesTotal int
+		var numBlocks int
 
-	reader := bufio.NewReader(os.Stdin)
-	numBytes, err := reader.Read(inputBuffer)
-	for err != io.EOF {
-		numBytesTotal += numBytes
-		numBytes, err = reader.Read(inputBuffer)
+		reader := bufio.NewReader(os.Stdin)
+		numBytes, err := reader.Read(inputBuffer)
+		for err != io.EOF {
+			numBytesTotal += numBytes
+			numBytes, err = reader.Read(inputBuffer)
 
-		// check if readBuffer is the last block in the buffer
-		lastBlock := false
-		_, err = reader.Peek(1)
-		if err == bufio.ErrNegativeCount {
-			lastBlock = true
+			// check if readBuffer is the last block in the buffer
+			isLastBlock := false
+			_, err = reader.Peek(1)
+			if err == bufio.ErrNegativeCount {
+				isLastBlock = true
+			}
+
+			numBlocks++
+
+			b := block{
+				Index:     numBlocks,
+				LastBlock: isLastBlock,
+				Data:      inputBuffer,
+			}
+			log.Println("read block#" + strconv.Itoa(b.Index))
+			out <- &b
 		}
+	}()
 
-		numBlocks++
-		out <- new(block{
-			index:     numBlocks,
-			lastBlock: false,
-			data:      inputBuffer,
-		})
-	}
 	return out
 }
 
 // Compress stage
-func compress(in <-chan block) <-chan block {
+func compress(in <-chan *block) <-chan *block {
+	out := make(chan *block)
+
+	go func() {
+		for b := range in {
+			log.Println("compressing block#" + strconv.Itoa(b.Index))
+			out <- b
+		}
+		close(out)
+	}()
+
+	return out
 }
 
 // Write stage
-func write(in <-chan block) {
+func write(b *block) {
+	log.Println("writing block#" + strconv.Itoa(b.Index))
 }
 
 // mergeList fans-in slice of results from the compress goroutines into the write stage
-func mergeSlice(compressOutbounds []<-chan block) <-chan block {
+func mergeSlice(compressOutbounds []<-chan *block) <-chan *block {
+	log.Println("merging")
+
+	var wg sync.WaitGroup
+	out := make(chan *block)
+
+	wg.Add(len(compressOutbounds))
+	for i := 0; i < len(compressOutbounds); i++ {
+		go func() {
+			for b := range compressOutbounds[i] {
+				out <- b
+			}
+			wg.Done()
+		}()
+	}
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	return out
 }
 
-func merge(cs ...<-chan block) <-chan block {
+func merge(cs ...<-chan *block) <-chan *block {
+	return nil
 }
 
 type block struct {
-	index     int
-	lastBlock bool
-	data      [BLOCK_SIZE]byte
-	crc32     [SUM_SIZE]byte
-	err       error
+	Index     int
+	LastBlock bool
+	Data      []byte
+	CRC32     []byte
+	Err       error
 }
